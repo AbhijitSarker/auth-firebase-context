cbdbb60d1a4ab5f469ba96777a99f3b0e2955e67
AbhijitSarker <abhijitsarker03@gmail.com>
2023-08-15 23:51:27 +0600

    login and register

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import { AuthContext } from './provider/AuthProvider';
 
 const Home = () => {
-    const user = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
+    console.log(user)
     return (
         <div>
             <h2>Home</h2>
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from './provider/AuthProvider';
 
 const Register = () => {
+    const { user, createUser } = useContext(AuthContext)
+    console.log(createUser)
+
     const handleRegister = (event) => {
         event.preventDefault();
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(name, email, password);
+
+
+        createUser(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                // console.log(loggedUser)
+                form.reset();
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
     return (
         <div className="hero min-h-screen bg-base-200">
